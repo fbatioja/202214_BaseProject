5edf6abfa34ebb41ab2881c3c212bbe07982521f
Fabian Batioja <f.batioja@uniandes.edu.co>
2022-09-16 23:34:04 -0500

    Pruebas postman

--- src/producto-tienda/producto-tienda.controller.ts
+++ src/producto-tienda/producto-tienda.controller.ts
@@ -7,17 +7,20 @@ import {
   Param,
   Post,
   Put,
+  UseInterceptors,
 } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
 import { TiendaDto } from 'src/tienda/tienda.dto';
 import { TiendaEntity } from 'src/tienda/tienda.entity';
 import { ProductoTiendaService } from './producto-tienda.service';
 
 @Controller('products')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class ProductoTiendaController {
   constructor(private readonly productoTiendaService: ProductoTiendaService) {}
 
-  @Post(':productoId/tiendas/:tiendaId')
+  @Post(':productoId/stores/:tiendaId')
   async addStoreToProduct(
     @Param('productoId') productoId: string,
     @Param('tiendaId') tiendaId: string,
@@ -28,23 +31,23 @@ export class ProductoTiendaController {
     );
   }
 
-  @Get(':productoId/tiendas/:tiendaId')
+  @Get(':productoId/stores/:tiendaId')
   async findStoreFromProduct(
     @Param('productoId') productoId: string,
     @Param('tiendaId') tiendaId: string,
   ) {
-    return await this.productoTiendaService.addStoreToProduct(
+    return await this.productoTiendaService.findStoreFromProduct(
       productoId,
       tiendaId,
     );
   }
 
-  @Get(':productoId/tiendas')
+  @Get(':productoId/stores')
   async findStoresFromProduct(@Param('productoId') productoId: string) {
     return await this.productoTiendaService.findStoresFromProduct(productoId);
   }
 
-  @Put(':productoId/tiendas')
+  @Put(':productoId/stores')
   async updateStoresFromProduct(
     @Body() tiendasDto: TiendaDto[],
     @Param('productoId') productoId: string,
@@ -56,7 +59,7 @@ export class ProductoTiendaController {
     );
   }
 
-  @Delete(':productoId/tiendas/:tiendaId')
+  @Delete(':productoId/stores/:tiendaId')
   @HttpCode(204)
   async deleteStoresFromProduct(
     @Param('productoId') productoId: string,
